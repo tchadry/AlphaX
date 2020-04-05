4619203a3353052d284558132b60d36f4e7c2114
Thomas Chadrycki <tchadrycki@gmail.com>
2020-04-05 16:05:30 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from Policy_GNN import GNN_Policy
 from MCTS_Node import MCTS_Node 
-from MCTSexample import MCTSexample
-from MCTS_solver import MCTS_solver
+from MCTSexample import MCTSExample
+from MCTS_solver import MCTS_Solver
 from MCTSGNNexample import MCTSGNNexample
 from GNN_Solver import GNNSolver
 from GNNMCTS_Solver import GNN_MCTS_Solver
@@ -9,6 +9,9 @@ from GNNTrainer import GNN_Trainer
 import torch
 from torch import nn
 from tsp import TSP
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
 
 training = []
 
@@ -20,5 +23,14 @@ loss_fn = nn.MSELoss()
 # generate examples
 for _ in range(25):
     tsp = TSP(20, 2)
-    solver = MCTSExampleGenerator(tsp, train_queue)
+    solver = MCTSExample(tsp, training)
     solver.solve()
+
+# train
+trainer = GNN_Trainer(policy_network, training)
+trainer.train_all()
+policy_network = trainer.model
+
+# plot loss
+plt.scatter(x=np.arange(len(trainer.losses)), y=trainer.losses, marker='.')
+plt.show()
