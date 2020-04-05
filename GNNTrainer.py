b2d436160d139b70eda6eab90a07a32d80cce596
Thomas Chadrycki <tchadrycki@gmail.com>
2020-04-05 13:29:51 -0400

    Create GNNTrainer.py

---
+++ GNNTrainer.py
@@ -0,0 +1,45 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.distributions.categorical import Categorical
+import torch_geometric
+from torch_geometric.nn import GCNConv, global_mean_pool
+from torch_geometric.data import Data, DataLoader
+
+class GNN_Trainer:
+    
+    def __init__(self, model, example_queue):
+        self.model = model
+        self.example_queue = example_queue
+        self.losses = []
+        
+    def train_example(self):
+        self.model.train()
+        
+        example = self.example_queue.get()
+        graph, choice_probs, value = example["graph"], example["choice_probs"], example["pred_value"]
+        
+        pred_choices, pred_value = self.model(graph)
+        loss = loss_fn(pred_choices, choice_probs) + (0.2 * loss_fn(pred_value, value))
+        
+        self.losses.append(loss.item())
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    def train_all(self):
+        self.model.train()
+        while not self.example_queue.empty():
+
+            example = self.example_queue.get()
+            graph, choice_probs, value = example["graph"], example["choice_probs"], example["pred_value"]
+            
+            pred_choices, pred_value = self.model(graph)
+            loss = loss_fn(pred_choices, choice_probs) + (0.2 * loss_fn(pred_value, value))
+            
+            self.losses.append(loss.item())
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
