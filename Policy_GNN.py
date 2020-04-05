e906991ca615eb0e23a026bee9684c9e9aeb17e6
felipegrabowsky <fm2579@columbia.edu>
2020-04-05 17:26:33 -0400

    debugging policy

--- Policy_GNN.py
+++ Policy_GNN.py
@@ -14,20 +14,23 @@ class GNN_Policy(nn.Module):
         self.conv2 = GCNConv(16,  16)
         self.conv3 = GCNConv(16, 1)
         self.activ = nn.Linear(16, 1)
-    
+
     def forward(self, data):
+        print("analyzing data")
+        print(data)
+        print(data.shape)
         x, edges, choices = data.x, data.edge_index, data.y
-        
+
         x = self.conv1(x, edges)
         x = F.relu(x)
         x = F.dropout(x)
         x = self.conv2(x, edges)
         x = F.relu(x)
-        
+
         c = self.conv3(x, edges)
         choice = torch.masked_select(c.squeeze(), choices)
         choice = F.softmax(choice, dim=0)
-        
+
         v = global_mean_pool(x, torch.zeros(data.num_nodes, dtype=data.long))
         value = self.activ(v)
 
